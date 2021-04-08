a4c2e426f96f56566a0b0e461dbeecdfd49b4578
Maia-Nguyen <maialyn@csu.fullerton.edu>
2021-04-08 14:16:13 -0700

    first commit

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -4,7 +4,7 @@
     var $ = window.jQuery;
 
     function FormHandler(selector) {
-        if (!selector) {
+       if (!selector) {
             throw new Error('No selector provided');
         }
 
@@ -31,7 +31,21 @@
         });
     };
 
+    FormHandler.prototype.addInputHandler = function (fn) {
+        console.log('Setting input handler for form');
+        this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+            var emailAddress = event.target.value;
+
+            var message = '';
+            if (fn(emailAddress)) {
+                    event.target.setCustomValidity('');
+            } else {
+                message = emailAddress + ' is not an authorized email address!'
+                event.target.setCustomValidity(message);
+            }
+        });
+    };
+
     App.FormHandler = FormHandler;
     window.App = App;
-
 })(window);
